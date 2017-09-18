620bb35b9dddf7c8b7eddb51e608fc4df371531d
sripanatula <panatula@hotmail.com>
2017-09-17 23:42:12 -0700

    My First Cargi checkin

---
+++ src/main/java/com/panatula/cargi/App.java
@@ -0,0 +1,13 @@
+package com.panatula.cargi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
