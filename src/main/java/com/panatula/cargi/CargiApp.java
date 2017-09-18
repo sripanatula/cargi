64816bf6fb33ef27568f361a78981aa7e6c8fde9
sripanatula <panatula@hotmail.com>
2017-09-18 00:00:17 -0700

    My second simle working jar

---
+++ src/main/java/com/panatula/cargi/CargiApp.java
@@ -0,0 +1,13 @@
+package com.panatula.cargi;
+
+/**
+ * Hello world!
+ *
+ */
+public class CargiApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Here we start the revolution of Automobile service automation" );
+    }
+}
---
+++ src/test/java/com/panatula/cargi/CargiAppTest.java
@@ -0,0 +1,15 @@
+package com.panatula.cargi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CargiAppTest 
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
